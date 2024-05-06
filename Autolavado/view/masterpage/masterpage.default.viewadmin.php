91ded1b63fbde75c6d58b715886108fcd827c575
N0oahs-C0 <n0oahs.5jp@gmail.com>
2024-05-06 17:34:45 -0600

    a

--- Autolavado/view/masterpage/masterpage.default.viewadmin.php
+++ Autolavado/view/masterpage/masterpage.default.viewadmin.php
@@ -14,18 +14,22 @@
 <body>
   <header class="header">
   <div class="contenedor contenido-header">
-    <nav class="">
-      <a class="buttonss" href="vehiculos">Vehiculos</a>
-      <a class="buttonss" href="pagarempleados">Pagar a empleados</a>
-      <a class="buttonss" href="empleados">Empleados</a>
-        <select class="buttonss" onchange="window.location.href=this.value">
-          <option class="options" value="#" selected>Registros</option>
-          <option class="options" value="empleadodia">Empleado del día</option>
-          <option class="options" value="clientesatendidos">Clientes atendidos</option>
-          <option class="options" value="pagos">Pagos Diarios</option>
-        </select>
-      <a class="exit buttonss" href="home">Cerrar Sesión</a>
+    <nav class="menu">
+        <a class="buttonss" href="vehiculos">Vehiculos</a>
+        <a class="buttonss" href="pagarempleados">Pagar a empleados</a>
+        <a class="buttonss" href="empleados">Empleados</a>
+        <div class="select-container">
+            <select class="buttonss select-custom" onchange="window.location.href=this.value">
+                <option class="options" value="#" selected disabled hidden>Registros</option>
+                <option class="options" value="empleadodia">Empleado del día</option>
+                <option class="options" value="clientesatendidos">Clientes atendidos</option>
+                <option class="options" value="pagos">Pagos Diarios</option>
+            </select>
+        </div>
+        <a class="exit buttonss" href="home">Cerrar Sesión</a>
     </nav>
+</div>
+
     </div>
     </header>
   </div>
